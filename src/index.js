437bb12389df0af7b6ad9e716a3777d100b39471
NishcheyBhutani <nishchay.bhutani3010@gmail.com>
2021-03-09 13:30:32 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const name='Nishchay';
+const currDate=new Date().toLocaleDateString();
+const currTime=new Date().toLocaleTimeString();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <>
+  <h1>Hello,My name is {name}</h1>
+  <p>Current Date is = {currDate}</p>
+  <p>Current Time is = {currTime}</p>
+  </>
+  ,document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
